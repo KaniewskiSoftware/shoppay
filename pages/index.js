87894d028ea69b3a441931b8b45dac25114b6e17
KaniewskiSoftware <kaniewski.patryk@gmail.com>
2023-05-23 18:32:23 +0200

    base scss connected as default import

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className={styles.red}>Hello from h1</h1>
     </>
   );
 }
