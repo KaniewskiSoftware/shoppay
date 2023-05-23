9af43e890dc9d6309dc1ef1c6b64d776a4747cab
KaniewskiSoftware <kaniewski.patryk@gmail.com>
2023-05-23 18:56:25 +0200

    temporary banner added to Header

---
+++ components/header/index.js
@@ -0,0 +1,10 @@
+import Ad from "./Ad";
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Ad />
+    </header>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,27 @@ import { Provider } from "react-redux";
 import store from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import Head from "next/head";
 
 let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Shoppay</title>
+        <meta
+          name="description"
+          content="Shoppay-online shopping service for all of your needs"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
