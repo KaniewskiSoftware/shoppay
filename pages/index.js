9037afc31f38e32b9e7b29ff0465835d3be6d65f
KaniewskiSoftware <kaniewski.patryk@gmail.com>
2023-05-24 19:20:34 +0200

    Footer created, Links added to footer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
