1367adbe5b2e6456d5643632c3f752a7e5978bce
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-05-28 14:41:15 +0200

    unused provider removed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import AppleProvider from "next-auth/providers/apple";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
