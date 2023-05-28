cbab4865a21b90366c95c19c6808cd2d2f9eda90
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-05-28 13:35:31 +0200

    signup front

--- pages/signin.js
+++ pages/signin.js
@@ -18,10 +18,21 @@ const country = {
 const initialValues = {
   login_email: "",
   login_password: "",
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
 };
 export default function signin({ providers }) {
   const [user, setUser] = useState(initialValues);
-  const { login_email, login_password } = user;
+  const {
+    login_email,
+    login_password,
+    name,
+    email,
+    password,
+    confirm_password,
+  } = user;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,6 +43,28 @@ export default function signin({ providers }) {
       .required("Email is required")
       .email("Please enter a valid email address."),
     login_password: Yup.string().required("Please enter a password."),
+  });
+
+  const registerValidation = Yup.object({
+    name: Yup.string()
+      .required("What's your name ?")
+      .min(2, "First name must be between 2 and 16 characters.")
+      .max(16, "First name must be between 2 and 16 characters.")
+      .matches(/^[aA-zZ]/, "Numbers and special characters are not allowed."),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if you ever need to reset your password."
+      )
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)."
+      )
+      .min(6, "Password must be atleast 6 characters.")
+      .max(36, "Password can't be more than 36 characters"),
+    confirm_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
   });
   return (
     <>
@@ -93,6 +126,51 @@ export default function signin({ providers }) {
                 ))}
               </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.login__container}>
+          <div className={styles.login__form}>
+            <h1>Sign Up</h1>
+            <p>Get access to one of the best Eshopping services in the world</p>
+            <Formik
+              enableReinitialize
+              initialValues={{ name, email, password, confirm_password }}
+              validationSchema={registerValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="name"
+                    icon="user"
+                    placeholder="Full Name"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="confirm_password"
+                    icon="password"
+                    placeholder="Confirm Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign Up" />
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
