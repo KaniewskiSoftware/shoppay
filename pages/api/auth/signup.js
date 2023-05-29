fb62b174e23eafb8304dd2b90a69f5ef18173b10
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-05-29 18:17:36 +0200

    response, db disconnect added

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -41,6 +41,10 @@ handler.post(async (req, res) => {
     });
     const url = `${process.env.BASE_URL}/activate/${activation_token}`;
     sendEmail(email, url, "", "Activate your account");
+    await db.disconnectDb();
+    res.json({
+      message: "Register success! Please activate your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
