e75068372973959293fda1be8e3570251b964e75
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-05-29 17:52:52 +0200

    auth email sending functionality added

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,10 @@
 import nc from "next-connect";
+import bcrypt from "bcrypt";
 import { validateEmail } from "@/utils/validation";
 import db from "../../../utils/db";
+import User from "@/models/User";
+import { createActivationToken } from "@/utils/tokens";
+import { sendEmail } from "@/utils/sendEmails";
 
 const handler = nc();
 
@@ -16,6 +20,27 @@ handler.post(async (req, res) => {
       res.status(400).json({ message: "Please enter a valid email" });
       return;
     }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This email already exists" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      name,
+      email,
+      password: cryptedPassword,
+    });
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    const url = `${process.env.BASE_URL}/activate/${activation_token}`;
+    sendEmail(email, url, "", "Activate your account");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
