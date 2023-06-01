da48d8b52a2088c218d24da2028344cd6c56a49d
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-06-01 20:42:26 +0200

    swiper added

--- home/main/index.js
+++ home/main/index.js
@@ -1,11 +1,12 @@
 import styles from "./styles.module.scss";
+import MainSwiper from "./swiper";
 
 export default function Main() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>header</div>
       <div className={styles.menu}>menu</div>
-      <div className={styles.swiper}>swiper</div>
+      <MainSwiper />
       <div className={styles.offers}>offers</div>
       <div className={styles.user}>user</div>
     </div>
